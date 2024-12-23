a10ba6a8cc88a9dae034842c47f1c004a844b073
Y-A-S-H-W-A-N-T <raoyashwant132@gmail.com>
2024-12-23 21:11:01 +0530

    Updated vite.config.js file

--- basic-review/react-weather-component/vite.config.js
+++ basic-review/react-weather-component/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // If you are running into issues with MIME type errors, try setting the following:
+    mimeTypes: {
+      '.js': 'application/javascript',
+    },
+  },
 })
