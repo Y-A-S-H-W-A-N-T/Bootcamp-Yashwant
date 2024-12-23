93d4f0c468c556753a33dde401f8f23586e88f20
Y-A-S-H-W-A-N-T <raoyashwant132@gmail.com>
2024-12-23 21:08:53 +0530

    Hosted (react-weather-component)

--- basic-review/react-weather-component/src/App.jsx
+++ basic-review/react-weather-component/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
 
   return (
     <>
+      <h2>
+      Reusable React weather Components 
+      <a href="../notes.html"> Read About this Component</a>
+      </h2>
       <WeatherComponent apiKey="b0b80ecb1ac34990a46201357242012" BG='linear-gradient(135deg, #6e8efb, #a777e3)'/>
     </>
   )
